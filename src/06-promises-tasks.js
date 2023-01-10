6bd6d8b09830200aa96bd5270d905b46d4ac5305
Aleh <oleh@tut.by>
2023-01-10 17:44:56 +0300

    6-function chainPromises

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -80,8 +80,8 @@ function processAllPromises(array) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
