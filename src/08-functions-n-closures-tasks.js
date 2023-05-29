54ad7788143fec8a58ca176a1eeeec2355197abf
Aleh <oleh@tut.by>
2023-05-29 17:40:52 +0300

    add solution

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,9 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  const res = (argument) => f(g(argument));
+  return res;
 }
 
 
